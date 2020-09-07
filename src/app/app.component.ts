169a886cf97e00d2297f185171de7fe79abf1f3c
Martin Peeters <406336@student.fontys.nl>
2020-09-07 13:47:18 +0200

    Implemented TailwindCSS as framework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'entryproj';
+
+  min = 0;
+  max = 20;
+  mid = parseFloat(((this.max - this.min) / 2).toFixed(5));
+  preHighLight = false;
+  nextHighLight = false;
+  _tempSliderValue = 0;
+
+  set tempSliderValue(value: number) {
+    this._tempSliderValue = value;
+    this.highlightIcon();
+  }
+
+  get tempSliderValue(): number {
+    return this._tempSliderValue;
+  }
+
+  ngOnInit(): void {
+    this.tempSliderValue = 0;
+  }
+
+  highlightIcon(): void {
+    const lower = this._tempSliderValue >= this.mid;
+    this.preHighLight = !lower;
+    this.nextHighLight = lower;
+  }
 }
